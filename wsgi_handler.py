cf8e1697c2f7728c255eb6c4075c8662b081b440
mattias <mattias.decharleroy@gmail.com>
2020-06-18 19:59:13 +0200

    Make serverless-wsgi work with azure, try

--- wsgi_handler.py
+++ wsgi_handler.py
@@ -11,6 +11,8 @@ import json
 import os
 import sys
 import traceback
+import logging
+import azure.functions as func
 
 # Call decompression helper from `serverless-python-requirements` if
 # available. See: https://github.com/UnitedIncome/serverless-python-requirements#dealing-with-lambdas-size-limitations
@@ -19,8 +21,8 @@ try:
 except ImportError:
     pass
 
-import serverless_wsgi
-
+root = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, root)
 
 def load_config():
     """ Read the configuration file created during deployment
@@ -49,72 +51,7 @@ def import_app(config):
         raise Exception("Unable to import {}".format(config["app"]))
 
 
-def append_text_mime_types(config):
-    """ Append additional text (non-base64) mime types from configuration file
-    """
-    if "text_mime_types" in config and isinstance(config["text_mime_types"], list):
-        serverless_wsgi.TEXT_MIME_TYPES.extend(config["text_mime_types"])
-
-
-def handler(event, context):
-    """ Lambda event handler, invokes the WSGI wrapper and handles command invocation
-    """
-    if "_serverless-wsgi" in event:
-        import shlex
-        import subprocess
-        from werkzeug._compat import StringIO, to_native
-
-        native_stdout = sys.stdout
-        native_stderr = sys.stderr
-        output_buffer = StringIO()
-
-        try:
-            sys.stdout = output_buffer
-            sys.stderr = output_buffer
-
-            meta = event["_serverless-wsgi"]
-            if meta.get("command") == "exec":
-                # Evaluate Python code
-                exec(meta.get("data", ""))
-            elif meta.get("command") == "command":
-                # Run shell commands
-                result = subprocess.check_output(
-                    meta.get("data", ""), shell=True, stderr=subprocess.STDOUT
-                )
-                output_buffer.write(to_native(result))
-            elif meta.get("command") == "manage":
-                # Run Django management commands
-                from django.core import management
-
-                management.call_command(*shlex.split(meta.get("data", "")))
-            elif meta.get("command") == "flask":
-                # Run Flask CLI commands
-                from flask.cli import FlaskGroup
-
-                flask_group = FlaskGroup(create_app=_create_app)
-                flask_group.main(
-                    shlex.split(meta.get("data", "")), standalone_mode=False
-                )
-            else:
-                raise Exception("Unknown command: {}".format(meta.get("command")))
-        except subprocess.CalledProcessError as e:
-            return [e.returncode, e.output.decode("utf-8")]
-        except:  # noqa
-            return [1, traceback.format_exc()]
-        finally:
-            sys.stdout = native_stdout
-            sys.stderr = native_stderr
-
-        return [0, output_buffer.getvalue()]
-    else:
-        return serverless_wsgi.handle_request(wsgi_app, event, context)
-
-
-def _create_app():
-    return wsgi_app
-
-
 # Read configuration and import the WSGI application
 config = load_config()
 wsgi_app = import_app(config)
-append_text_mime_types(config)
+main = func.WsgiMiddleware(wsgi_app).main
